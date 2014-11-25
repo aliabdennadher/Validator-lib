859ac2fbfc00a4f2b990715f9ade040b9dc099d2
Ali Abdennadher <aliabdennadher@hotmail.fr>
2014-11-25 22:22:13 +0100

    uploader4

--- src/BooleanValidator.php
+++ src/BooleanValidator.php
@@ -35,5 +35,23 @@ class BooleanValidator
         }
     }
 
-
+    /**
+     * @param bool $boolean
+     *
+     * @return bool
+     *
+     * @throws \Exception
+     */
+    public static function isFalse($boolean)
+    {
+        if (!is_bool($boolean)) {
+            throw new \Exception('$boolean must be a boolean');
+        }else {
+            if (!filter_var($boolean, FILTER_VALIDATE_BOOLEAN, ['flags' => FILTER_NULL_ON_FAILURE])) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
